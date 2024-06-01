ecff32e42e0fe2787cddac007f8997f3267cc955
David.McGregor1 <david@plisky.net>
2024-06-01 13:45:42 +0100

    Added main game method in game service

--- src/main/java/com/example/demo/PdDungeonGameApplication.java
+++ src/main/java/com/example/demo/PdDungeonGameApplication.java
@@ -1,5 +1,6 @@
 package com.example.demo;
 
+import com.example.demo.Services.GameService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,6 +9,8 @@ public class PdDungeonGameApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PdDungeonGameApplication.class, args);
+		GameService gameService = new GameService();
+		gameService.startGame();
 	}
 
 }
--- src/main/java/com/example/demo/Services/GameService.java
+++ src/main/java/com/example/demo/Services/GameService.java
@@ -1,4 +1,49 @@
 package com.example.demo.Services;
 
+import com.example.demo.Views.Room;
+
+import java.util.Scanner;
+
 public class GameService {
+
+    RoomService roomService = new RoomService();
+    private Scanner scanner;
+    Room currentRoom;
+
+    public void startGame() {
+        System.out.println("Game started!");
+
+        currentRoom = roomService.createRooms();
+        boolean gameRunning = true;
+        scanner = new Scanner(System.in);
+
+        while (gameRunning) {
+            System.out.println("You are currently in " + currentRoom.getName() + ". Choose a direction (N, E, S, W):");
+            scanner = new Scanner(System.in);
+            String input = scanner.nextLine();
+
+            if (input.equals("exit")) {
+                gameRunning = false;
+                System.out.println("Game over!");
+                break;
+            } else {
+                movePlayer(input);
+            }
+        }
+
+
+
+    }
+
+    public void movePlayer(String direction) {
+        Room nextRoom = currentRoom.getNeighbour(direction);
+
+        if (nextRoom != null) {
+            currentRoom = nextRoom;
+            System.out.println("You have moved to " + currentRoom.getName() + ".");
+        } else {
+            System.out.println("You cannot move in that direction.");
+        }
+    }
+
 }
