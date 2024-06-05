4062bd0074fb76ec1ab91398767b6b8f427d6e53
David McGregor <david@plisky.net>
2024-06-05 21:50:52 +0100

    added player set up class and added attack tests

--- src/test/java/com/example/demo/TestSetup/PlayerSetup.java
+++ src/test/java/com/example/demo/TestSetup/PlayerSetup.java
@@ -1,4 +1,16 @@
 package com.example.demo.TestSetup;
 
+import com.example.demo.Player.Player;
+import com.example.demo.Weapon.Weapon;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class PlayerSetup {
+    public Player createMockPlayer() {
+        Weapon knife = new Weapon("Knife", 10);
+        List inventory = new ArrayList();
+        Player player = new Player("Test Player", 100, 1, knife, inventory);
+        return player;
+    }
 }
--- src/test/java/com/example/demo/UnitTests/AttackTests.java
+++ src/test/java/com/example/demo/UnitTests/AttackTests.java
@@ -1,4 +1,15 @@
 package com.example.demo.UnitTests;
 
+import com.example.demo.Monster.Monster;
+import com.example.demo.Player.Player;
+import com.example.demo.TestSetup.PlayerSetup;
+import org.junit.jupiter.api.Test;
+
 public class AttackTests {
+    PlayerSetup playerSetup = new PlayerSetup();
+    @Test
+    public void playerCanAttackMonster() {}
+    Player player = playerSetup.createMockPlayer();
+    Monster monster = new Monster("Test Monster", 100, 2, 10);
+
 }
