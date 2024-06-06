23121776320a1c6777dfcf50321f856a3b864e8c
David.McGregor1 <david@plisky.net>
2024-06-06 18:32:41 +0100

    fixed failing test and added run tests to pipeline

--- src/test/java/com/example/demo/TestSetup/PlayerSetup.java
+++ src/test/java/com/example/demo/TestSetup/PlayerSetup.java
@@ -6,9 +6,11 @@ import com.example.demo.Weapon.Weapon;
 import java.util.ArrayList;
 import java.util.List;
 
+import static com.example.demo.Weapon.WeaponRarity.COMMON;
+
 public class PlayerSetup {
     public Player createMockPlayer() {
-        Weapon knife = new Weapon("Knife", 10);
+        Weapon knife = new Weapon("Knife", 10, COMMON);
         List inventory = new ArrayList();
         Player player = new Player("Test Player", 100, 1, knife, inventory);
         return player;
