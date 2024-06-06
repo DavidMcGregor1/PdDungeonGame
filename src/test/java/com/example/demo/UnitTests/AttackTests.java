89df2625645d8b2b82d771ac6be8b5132db2c881
David McGregor <david@plisky.net>
2024-06-06 21:27:49 +0100

    added given when then syntax and created a new unit test

--- src/test/java/com/example/demo/UnitTests/AttackTests.java
+++ src/test/java/com/example/demo/UnitTests/AttackTests.java
@@ -9,14 +9,32 @@ public class AttackTests {
     PlayerSetup playerSetup = new PlayerSetup();
     @Test
     public void playerCanAttackMonster() {
-
+        // given
         Player player = playerSetup.createMockPlayer();
         Monster monster = new Monster("Test Monster", 100, 2, 10);
         int monsterHealth = monster.getHealth();
-        player.attack(player, monster);
         int expectedHealth = monsterHealth - player.getWeapon().getDamage();
-        assert(monster.getHealth() == expectedHealth);
 
+        // when
+        player.attack(player, monster);
+
+        // then
+        assert(monster.getHealth() == expectedHealth);
+    }
+
+    @Test
+    public void monsterCanAttackPlayer() {
+        // given
+        Player player = playerSetup.createMockPlayer();
+        Monster monster = new Monster("Test Monster", 100, 2, 10);
+        int playerHealth = player.getHealth();
+        int expectedHealth = playerHealth - monster.getDamage();
+
+        // when
+        monster.attack(player, monster);
+
+        // then
+        assert(player.getHealth() == expectedHealth);
     }
 
 
