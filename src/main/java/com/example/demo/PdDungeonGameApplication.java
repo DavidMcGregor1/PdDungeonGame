2dec1e7ecd1e8203c86d6a6fb1f98714c3ff1565
David McGregor <david@plisky.net>
2024-06-07 15:52:57 +0100

    logger logs to console

--- src/main/java/com/example/demo/PdDungeonGameApplication.java
+++ src/main/java/com/example/demo/PdDungeonGameApplication.java
@@ -1,17 +1,22 @@
 package com.example.demo;
 
 import com.example.demo.Game.GameService;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class PdDungeonGameApplication {
 
+	private final static Logger LOGGER = LogManager.getLogger(PdDungeonGameApplication.class);
+
 	public static void main(String[] args) {
 		SpringApplication.run(PdDungeonGameApplication.class, args);
 //		GameService gameService = new GameService();
 //		gameService.startGame();
 		System.out.println("working");
+		LOGGER.info("here");
 	}
 
 }
