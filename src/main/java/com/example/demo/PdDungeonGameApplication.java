69f65526f300a20f1a0c1216cd8b935d01de1fd7
David McGregor <david@plisky.net>
2024-06-08 20:44:36 +0100

    got logging working!!!

--- src/main/java/com/example/demo/PdDungeonGameApplication.java
+++ src/main/java/com/example/demo/PdDungeonGameApplication.java
@@ -3,6 +3,7 @@ package com.example.demo;
 import com.example.demo.Game.GameService;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.springframework.boot.logging.log4j2.*;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -17,6 +18,7 @@ public class PdDungeonGameApplication {
 //		gameService.startGame();
 		System.out.println("working");
 		LOGGER.info("here");
+		LOGGER.debug("debug");
 	}
 
 }
